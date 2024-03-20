e702aff8763c4cae1d00666638c940065b3fa5a0
IsraCoder <IZughyer@gmail.com>
2024-03-20 13:53:27 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:test_proj/firebase_options.dart';
+import 'package:test_proj/views/login_view.dart';
+
+void main() {
+  // runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const LoginView(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      body: FutureBuilder(
+          future: Firebase.initializeApp(
+              options: DefaultFirebaseOptions.currentPlatform),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false)
+                  print('You are a verified user');
+                else
+                  print('You need to verify your email first');
+                return const Text('Done');
+              default:
+                return const Text('Loading');
+            }
+          }),
+    );
+  }
+}
