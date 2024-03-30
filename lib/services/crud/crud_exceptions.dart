9f3bf88af7c4e3d29d80e6196e20a7729f30646e
IsraCoder <IZughyer@gmail.com>
2024-03-30 17:26:52 +0200

    step18-Connecting getAllNotes to specific User

--- lib/services/crud/crud_exceptions.dart
+++ lib/services/crud/crud_exceptions.dart
@@ -15,3 +15,5 @@ class CouldNotUpdateNote implements Exception {}
 class CouldNotFindNote implements Exception {}
 
 class UserAlreadyExists implements Exception {}
+
+class UserShouldBeSetBeforeReadingAllNotes implements Exception {}
--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -8,10 +8,12 @@ import 'package:path_provider/path_provider.dart'
     show MissingPlatformDirectoryException, getApplicationDocumentsDirectory;
 import 'package:sqflite/sqflite.dart';
 import 'package:test_proj/constants/routes.dart';
+import 'package:test_proj/extensions/list/filter.dart';
 import 'package:test_proj/services/crud/crud_exceptions.dart';
 
 class NotesService {
   Database? _db;
+  DatabaseUser? _user;
   List<DatabaseNote> _notes = [];
   // creating a singleton for the notes service
   // to ensure only one copy for the entire application
@@ -26,7 +28,14 @@ class NotesService {
 
   late final StreamController<List<DatabaseNote>> _notesStreamController;
 
-  Stream<List<DatabaseNote>> get allNotes => _notesStreamController.stream;
+  Stream<List<DatabaseNote>> get allNotes =>
+      _notesStreamController.stream.filter((note) {
+        final currentUser = _user;
+        if (currentUser != null) {
+          return note.userId == currentUser.id;
+        } else
+          throw UserShouldBeSetBeforeReadingAllNotes();
+      });
 
   Future<void> _cacheNotes() async {
     final allNotes = await getAllNotes();
@@ -203,10 +212,15 @@ class NotesService {
     await _ensureDbIsOpen();
     final db = _getDatabaseOrThrow();
     await getNote(id: note.id);
-    final updatesCount = await db.update(noteTable, {
-      textColumn: text,
-      isSyncedColumn: 0,
-    });
+    final updatesCount = await db.update(
+      noteTable,
+      {
+        textColumn: text,
+        isSyncedColumn: 0,
+      },
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
     } else {
@@ -218,12 +232,19 @@ class NotesService {
     }
   }
 
-  Future<DatabaseUser> getOrCreateUser({required String email}) async {
+  Future<DatabaseUser> getOrCreateUser({
+    required String email,
+    bool setAsCurrentUser = true,
+  }) async {
     try {
       final user = await getUser(email: email);
+      if (setAsCurrentUser) _user = user;
       return user;
     } on CouldNotFindUser {
       final createdUser = await createUser(email: email);
+      if (setAsCurrentUser) {
+        _user = createdUser;
+      }
       return createdUser;
     } catch (e) {
       rethrow;
