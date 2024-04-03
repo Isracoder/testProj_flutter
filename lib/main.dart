ca22a71450a6d72db07f73dfb214b33e500346e9
IsraCoder <IZughyer@gmail.com>
2024-04-03 14:38:04 +0200

    step26-Loading screen in place of dialog

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_proj/constants/routes.dart';
 import 'package:test_proj/firebase_options.dart';
+import 'package:test_proj/helpers/loading/loading_screen.dart';
 import 'package:test_proj/services/auth/auth_service.dart';
 import 'package:test_proj/services/auth/bloc/auth_bloc.dart';
 import 'package:test_proj/services/auth/bloc/auth_event.dart';
@@ -48,7 +49,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<AuthBloc>().add(const AuthEventInitialize());
 
-    return BlocBuilder(builder: (context, state) {
+    return BlocConsumer<AuthBloc, AuthState>(listener: (context, state) {
+      if (state.isLoading) {
+        LoadingScreen().show(
+          context: context,
+          text: state.loadingText ?? 'Please wait a moment',
+        );
+      } else
+        LoadingScreen().hide();
+    }, builder: (context, state) {
       if (state is AuthStateLoggedIn) {
         return const NotesView();
       } else if (state is AuthStateNeedsVerification)
--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -4,20 +4,24 @@ import 'package:test_proj/services/auth/auth_user.dart';
 
 @immutable
 abstract class AuthState {
-  const AuthState();
+  final bool isLoading;
+  final String? loadingText;
+
+  const AuthState({
+    required this.isLoading,
+    this.loadingText = 'Please wait a moment',
+  });
 }
 
-// class AuthStateLoading extends AuthState {
-//   const AuthStateLoading();
-// }
-
 class AuthStateUninitialized extends AuthState {
-  const AuthStateUninitialized();
+  const AuthStateUninitialized({required bool isLoading})
+      : super(isLoading: isLoading);
 }
 
 class AuthStateLoggedIn extends AuthState {
   final AuthUser user;
-  const AuthStateLoggedIn(this.user);
+  const AuthStateLoggedIn({required this.user, required bool isLoading})
+      : super(isLoading: isLoading);
 }
 
 // class AuthStateLoginFailure extends AuthState {
@@ -26,14 +30,16 @@ class AuthStateLoggedIn extends AuthState {
 // }
 
 class AuthStateNeedsVerification extends AuthState {
-  const AuthStateNeedsVerification();
+  const AuthStateNeedsVerification({required bool isLoading})
+      : super(isLoading: isLoading);
 }
 
 class AuthStateLoggedOut extends AuthState with EquatableMixin {
   final Exception? exception;
-  final bool isLoading;
 
-  const AuthStateLoggedOut({required this.exception, required this.isLoading});
+  const AuthStateLoggedOut(
+      {required this.exception, required bool isLoading, String? loadingText})
+      : super(isLoading: isLoading, loadingText: loadingText);
 
   @override
   List<Object?> get props => [exception, isLoading];
@@ -46,5 +52,6 @@ class AuthStateLoggedOut extends AuthState with EquatableMixin {
 
 class AuthStateRegistering extends AuthState {
   final Exception? exception;
-  const AuthStateRegistering(this.exception);
+  const AuthStateRegistering({required this.exception, required bool isLoading})
+      : super(isLoading: isLoading);
 }
